85eb1f14da12168a11ddbc65f2224aa7f7b16f39
Luke Shepherd <stnvlms@ucl.ac.uk>
2020-11-02 21:58:19 +0000

    new nbs

--- PCA_2/_nbdev.py
+++ PCA_2/_nbdev.py
@@ -4,7 +4,11 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"bone": "00_bone.ipynb",
          "mag": "01_maths.ipynb",
-         "angle": "01_maths.ipynb"}
+         "angle": "01_maths.ipynb",
+         "quaternion_rotation_from_angle": "02_rotation.ipynb",
+         "quaternion_rotation_from_quaternion": "02_rotation.ipynb",
+         "rotate": "02_rotation.ipynb",
+         "df_angles": "03_table.ipynb"}
 
 modules = ["core.py"]
 
